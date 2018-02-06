7ed64178a739563922bfee0a5042c110c40f9e63
Christian Cameron <christiancameron@cslab05.wheaton.edu>
2018-02-05 18:31:06 -0600

    Added constructor, convert array to heap

--- impl/HeapSorter.java
+++ impl/HeapSorter.java
@@ -17,20 +17,29 @@ import java.util.Comparator;
 public class HeapSorter extends Heap<Integer> {
 
 
+	
     /**
      * Constructor. Take an array an sets it up as a (max-) heap.
      * @param internal The array to be used for the internal representation.
      */
     private HeapSorter(int[] array) {
-        internal = new Integer[array.length];
-        for (int i = 0; i < array.length; i++)
-            internal[i] = array[i];
-        heapSize = array.length;
-        
-         //fix this; set compy to an appropriate comparator
-        
-        // insert code for rearranging this as a heap
-        
+		internal = new Integer[array.length];
+		for (int i = 0; i < array.length; i++)
+			internal[i] = array[i];
+		heapSize = array.length;
+		
+		compy = new Comparator<Integer>() {
+			@Override
+			public int compare(Integer o1, Integer o2) {
+				return o1.compareTo(o2);
+			}
+		};
+
+		for(int i = 0; i < heapSize; i++) {
+			
+			super.sinkKeyAt(i);
+		}
+
     }
     
     /**
@@ -38,21 +47,16 @@ public class HeapSorter extends Heap<Integer> {
      * @param array The array to sort.
      */
     public static void sort(int[] array) {
-    
-        HeapSorter heap = new HeapSorter(array);
 
-        
-        // insert code for completing the heap sort algorithm,
-        // with post condition that heap.internal is sorted
-        
+		HeapSorter heap = new HeapSorter(array);
 
-        
-        
-        // copy elements from internal (now sorted) back to array
-        for (int i = 0; i < array.length; i++)
-            array[i] = heap.internal[i];
-        
-        
+		// insert code for completing the heap sort algorithm,
+		// with post condition that heap.internal is sorted
+
+		// copy elements from internal (now sorted) back to array
+		for (int i = 0; i < array.length; i++)
+			array[i] = heap.internal[i];
+
     }
 
 }
